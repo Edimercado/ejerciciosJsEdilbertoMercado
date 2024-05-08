8b92f3e6c6f92d04940ae9cb788360b9cbceb082
Your NameEdilbertoMercado <edimercado07@gmail.com>
2024-05-08 10:50:48 -0500

    agregando ejercicios eventos

---
+++ ejerciciosEventos/xxxx.js
@@ -0,0 +1,37 @@
+
+// let calcularIMC = document.getElementById("calcularIMC");
+
+// calcularIMC.addEventListener("click", function () {
+//     let peso = parseFloat(document.getElementById("peso").value);
+//     let altura = parseFloat(document.getElementById("altura").value);
+//     let resultado = document.getElementById("resultado");
+//     if (peso <= 0 || altura <= 0) {
+//         alert("Por favor ingresa valores válidos para peso y altura.")
+//         return;
+//     }
+
+//     let imc = peso / (altura * altura);
+
+//     resultado.value = imc.toFixed(2);
+
+// })
+
+
+let pesoColombianoInput = document.getElementById("pesoColombiano")
+let dolarEstadounidenseInput = document.getElementById("dolar")
+
+pesoColombianoInput.addEventListener("input",function () {
+    let pesoColombiano = parseFloat(pesoColombianoInput.value);
+    let dolarEstadounidense=pesoColombiano/3893.79;
+
+    dolarEstadounidenseInput.value = dolarEstadounidense.toFixed(2);
+    
+});
+
+dolarEstadounidenseInput.addEventListener("input",function () {
+    let dolarEstadounidense = parseFloat(dolarEstadounidenseInput.value);
+    let pesoColombiano=dolarEstadounidense*3893.79;
+
+    pesoColombianoInput.value = pesoColombiano.toFixed(2);
+    
+})
